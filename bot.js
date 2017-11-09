b3263a996206efbcf8e5ba36c527fcdbbf55f901
Alejandro Mesa <alejandro.mesa1991@gmail.com>
2017-11-09 11:10:24 -0500

    comments

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Twit = require('twit');
 const TwitterBot = require('node-twitterbot').TwitterBot;
 const fetch = require('node-fetch');
 
+// Write the Env variables on Heroku or your server, to keep them save from Github
 var Bot = new TwitterBot({
   consumer_key: process.env.BOT_CONSUMER_KEY,
   consumer_secret: process.env.BOT_CONSUMER_SECRET,
@@ -9,6 +10,7 @@ var Bot = new TwitterBot({
   access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 });
 
+// Function that builds and tweets the message
 function runBot() {
   fetch('https://api.coinmarketcap.com/v1/ticker/bitcoin/')
     .then(resp => resp.json())
@@ -29,8 +31,9 @@ Remaining Supply: ${(100 - info.total_supply / info.max_supply * 100).toFixed(
         : info.percent_change_7d + '% 📉'}`
     )
     .then(bitcoin => Bot.tweet(bitcoin))
-    //.then(bitcoin => console.log(bitcoin))
+    //.then(bitcoin => console.log(bitcoin)) // debugging purposes
     .catch(error => console.log('Error ', error));
 }
 
+// run Bot
 runBot();
